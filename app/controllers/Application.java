ea8ce500ecc62c9038168bd3065c732e769eef18
takumi.murai <takumi.murai@rakus.co.jp>
2017-10-30 12:37:26 +0900

    対話型アプリケーション実装

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,18 +1,11 @@
 package controllers;
 
-import play.*;
 import play.mvc.*;
-import play.mvc.Http.Context;
-import models.*;
 
 import views.html.*;
-import play.mvc.Http.MultipartFormData;
 import play.data.*;
-import play.data.Form.*;
-import java.util.*;
 
 import javax.inject.Inject;
-import javax.inject.Singleton;
 
 import controllers.forms.SampleForm;
 
@@ -23,18 +16,17 @@ public class Application extends Controller {
     private Form<SampleForm> form;
 
     @Inject
-    public Application(FormFactory formfaFactory) {
-        this.form = formfaFactory.form(SampleForm.class);
+    public Application(FormFactory formFactory) {
+        this.form = formFactory.form(SampleForm.class);
     }
 
     public Result index() {
         return ok(index.render("何か書いて", form));
     }
-    // Form<SampleForm> requestForm = form.bindFromRequest();
     
     public Result send() {
-        Form<SampleForm> f;
-        return null;
+        SampleForm sampleForm = form.bindFromRequest().get();
+        return ok(index.render(sampleForm.getMessage(), form.fill(sampleForm)));        
     }
 
 }
